969c3937c2bdd86ab1b13b0f7aaa5d8d9da100f8
Javier García <develop@javier.games>
2024-09-18 15:47:04 +0900

    Updated intro background.

---
+++ resources/ui-intro-transformation.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="ui-intro-transformation" tilewidth="8" tileheight="8" tilecount="390" columns="30">
+ <image source="ui-intro-transformation.png" width="240" height="208"/>
+</tileset>
