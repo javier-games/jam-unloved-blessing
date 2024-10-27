90d89a4de6c3e3fe77089a0ae3d751ef769db9fd
Javier García <develop@javier.games>
2024-10-27 11:46:41 +0900

    Breaking change reset.

--- resources/tilesets/ui/ui-intro-tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="intro-tileset" tilewidth="16" tileheight="16" tilecount="160" columns="10">
- <image source="../../../assets/backgrounds/ui-intro.png" width="160" height="256"/>
-</tileset>
